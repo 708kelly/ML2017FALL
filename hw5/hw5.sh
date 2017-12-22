15c0d7b4cef0fd401f715090aa58c87fa5e00e87
708kelly <32258790+708kelly@users.noreply.github.com>
2017-12-22 22:27:53 +0800

    Update hw5.sh

--- hw5/hw5.sh
+++ hw5/hw5.sh
@@ -1,2 +1,2 @@
-wget -O hw5_5_1.h5 "https://www.dropbox.com/s/oxvgegbe8okwn02/hw5_5_1.h5?dl=1"
+#wget -O hw5_5_1.h5 "https://www.dropbox.com/s/oxvgegbe8okwn02/hw5_5_1.h5?dl=1"
 python3 test.py $1 $2
